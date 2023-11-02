4ed6ae5aa290cb1ca23466b45e160a8d5bbb5fe3
eusoubrian <brian.gomes@sptech.school>
2023-11-02 17:09:38 -0300

    Criando as duas funções de logs e adicionando uns try--catch no código para registrar os logs

--- src/main/java/br/com/sptech/modelo/banco/jdbc/App.java
+++ src/main/java/br/com/sptech/modelo/banco/jdbc/App.java
@@ -21,10 +21,47 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class App {
+    private static void log(String message) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String timestamp = sdf.format(new Date());
+            String logMessage = timestamp + " - " + message;
+
+            FileWriter fw = new FileWriter("C:\\Users\\brian\\yotte-monitoramento-grupo\\monitoramento.log", true); // Nome do arquivo de log
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write(logMessage);
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void logError(String errorMessage, Exception exception) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String timestamp = sdf.format(new Date());
+            String logMessage = timestamp + " - " + errorMessage + ": " + exception.getMessage();
+
+            FileWriter fw = new FileWriter("C:\\Users\\brian\\yotte-monitoramento-grupo\\error_log.txt", true); // Nome do arquivo de log de erros
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write(logMessage);
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         // Crie um agendador de tarefas
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
@@ -38,9 +75,13 @@ public class App {
         Scanner leitor = new Scanner(System.in);
         Scanner leitorTexto = new Scanner(System.in);
 
+
+
         Boolean logado = false;
 
         ValidacoesUsuario validacoesUsuario = new ValidacoesUsuario();
+
+
 
         System.out.println("""
                         :::   :::  ::::::::  ::::::::::: ::::::::::: ::::::::::
@@ -74,75 +115,87 @@ public class App {
 
 
             do {
-                System.out.println("Fazer cadastro...");
+                try {
+                    System.out.println("Fazer cadastro...");
 
-                System.out.println("Digite seu nome:");
-                nome = leitorTexto.nextLine();
+                    System.out.println("Digite seu nome:");
+                    nome = leitorTexto.nextLine();
 
-                System.out.println("Digite seu email:");
-                email = leitorTexto.nextLine();
+                    System.out.println("Digite seu email:");
+                    email = leitorTexto.nextLine();
 
-                System.out.println("Digite sua senha:");
-                senha = leitorTexto.nextLine();
+                    System.out.println("Digite sua senha:");
+                    senha = leitorTexto.nextLine();
 
-                System.out.println("Digite seu área de atuação:");
-                area = leitorTexto.nextLine();
+                    System.out.println("Digite seu área de atuação:");
+                    area = leitorTexto.nextLine();
 
-                System.out.println("Digite seu cargo:");
-                cargo = leitorTexto.nextLine();
+                    System.out.println("Digite seu cargo:");
+                    cargo = leitorTexto.nextLine();
 
-                System.out.println("Digite sua empresa:");
-                empresa = leitorTexto.nextLine();
+                    System.out.println("Digite sua empresa:");
+                    empresa = leitorTexto.nextLine();
 
-                System.out.println("Digite seu IP: ");
-                ip = leitorTexto.nextLine();
+                    System.out.println("Digite seu IP: ");
+                    ip = leitorTexto.nextLine();
 
-                System.out.println("Digite o modelo do notebook: ");
-                modelo = leitorTexto.nextLine();
+                    System.out.println("Digite o modelo do notebook: ");
+                    modelo = leitorTexto.nextLine();
 
-                System.out.println("Digite qual SO você utiliza: ");
-                so = leitorTexto.nextLine();
+                    System.out.println("Digite qual SO você utiliza: ");
+                    so = leitorTexto.nextLine();
 
-                System.out.println("Digite seu token de acesso: ");
-                matricula = leitorTexto.nextLine();
+                    System.out.println("Digite seu token de acesso: ");
+                    matricula = leitorTexto.nextLine();
 
-                Boolean isSenhaValida = validacoesUsuario.isSenhaValida(senha);
-                Boolean isSenhaComplexa = validacoesUsuario.isSenhaComplexa(senha);
-                Boolean emailNaoTemEspacos = validacoesUsuario.naoTemEspacos(email);
-                Boolean isEmailValido = validacoesUsuario.isEmailValido(email);
+                    Boolean isSenhaValida = validacoesUsuario.isSenhaValida(senha);
+                    Boolean isSenhaComplexa = validacoesUsuario.isSenhaComplexa(senha);
+                    Boolean emailNaoTemEspacos = validacoesUsuario.naoTemEspacos(email);
+                    Boolean isEmailValido = validacoesUsuario.isEmailValido(email);
 
-                if (isSenhaValida && emailNaoTemEspacos && isEmailValido && isSenhaComplexa) {
-                    todasValidacoes = true;
+                    if (isSenhaValida && emailNaoTemEspacos && isEmailValido && isSenhaComplexa) {
+                        todasValidacoes = true;
 
-                    if (usuarioDao.isTokenValido(matricula)) {
-                        if (usuarioDao.buscarEmpresaPorNome(empresa) != null) {
-                            Integer fkEmpresa = usuarioDao.buscarEmpresaPorNome(empresa);
+                        try {
+                            if (usuarioDao.isTokenValido(matricula)) {
+                                if (usuarioDao.buscarEmpresaPorNome(empresa) != null) {
+                                    Integer fkEmpresa = usuarioDao.buscarEmpresaPorNome(empresa);
 
-                            novoUsuario.setNome(nome);
-                            novoUsuario.setEmail(email);
-                            novoUsuario.setSenha(senha);
-                            novoUsuario.setArea(area);
-                            novoUsuario.setCargo(cargo);
-                            novoUsuario.setFkEmpresa(fkEmpresa);
-                            novoUsuario.setFkTipoUsuario(3);
+                                    novoUsuario.setNome(nome);
+                                    novoUsuario.setEmail(email);
+                                    novoUsuario.setSenha(senha);
+                                    novoUsuario.setArea(area);
+                                    novoUsuario.setCargo(cargo);
+                                    novoUsuario.setFkEmpresa(fkEmpresa);
+                                    novoUsuario.setFkTipoUsuario(3);
 
-                            MaquinaDao maquinaDao = new MaquinaDao();
-                            ModelMaquina novaMaquina = new ModelMaquina();
-                            novaMaquina.setIp(ip);
-                            novaMaquina.setSo(so);
-                            novaMaquina.setModelo(modelo);
+                                    MaquinaDao maquinaDao = new MaquinaDao();
+                                    ModelMaquina novaMaquina = new ModelMaquina();
+                                    novaMaquina.setIp(ip);
+                                    novaMaquina.setSo(so);
+                                    novaMaquina.setModelo(modelo);
 
-                            usuarioDao.salvarUsuario(novoUsuario);
-                            maquinaDao.salvarMaquina(novaMaquina, usuarioDao.buscarIdUsuario(novoUsuario), usuarioDao.buscarIdToken(matricula));
-                            maquina01.buscarIdMaquina(usuarioDao.buscarIdUsuario(novoUsuario));
+                                    usuarioDao.salvarUsuario(novoUsuario);
+                                    maquinaDao.salvarMaquina(novaMaquina, usuarioDao.buscarIdUsuario(novoUsuario), usuarioDao.buscarIdToken(matricula));
+                                    maquina01.buscarIdMaquina(usuarioDao.buscarIdUsuario(novoUsuario));
 
-                            logado = true;
+                                    logado = true;
+                                    log("Cadastro bem-sucedido para o usuário " + nome);
+                                    log("Área de atuação " + area);
+                                    log("Sistema Operacional" + so);
+                                }
+                            } else {
+                                System.out.println("Seu token não é válido!");
+                            }
+                        } catch (Exception e) {
+                            logError("Erro durante o processo de cadastro", e);
                         }
+                        log("Email e senha válidas " + email);
                     } else {
-                        System.out.println("Seu token não é válido!");
+                        System.out.println("Dados inválidos, faça o cadastro novamente!!");
                     }
-                } else {
-                    System.out.println("Dados inválidos, faça o cadastro novamente!!");
+                } catch (Exception e) {
+                    logError("Erro durante a verificação de senha e email", e);
                 }
             } while (!todasValidacoes);
         } else if (opcao == 1) {
@@ -152,26 +205,31 @@ public class App {
             String validarSenha;
 
             do {
-                ModelUsuario usuario = new ModelUsuario();
-                System.out.println("Digite seu email:");
-                validarEmail = leitorTexto.nextLine();
-                System.out.println("Digite sua senha:");
-                validarSenha = leitorTexto.nextLine();
+                try {
+                    ModelUsuario usuario = new ModelUsuario();
+                    System.out.println("Digite seu email:");
+                    validarEmail = leitorTexto.nextLine();
+                    System.out.println("Digite sua senha:");
+                    validarSenha = leitorTexto.nextLine();
 
-                usuario.setEmail(validarEmail);
-                usuario.setSenha(validarSenha);
+                    usuario.setEmail(validarEmail);
+                    usuario.setSenha(validarSenha);
 
-                if (usuarioDao.isUsuarioExistente(usuario)) {
+                    if (usuarioDao.isUsuarioExistente(usuario)) {
+                        todasValidacoesLogin = true;
+                        System.out.println("Id usuario: " + usuarioDao.buscarIdUsuario(usuario));
+                        maquina01.buscarIdMaquina(usuarioDao.buscarIdUsuario(usuario));
+                        logado = true;
 
-                    todasValidacoesLogin = true;
-                    System.out.println("Id usuario: " + usuarioDao.buscarIdUsuario(usuario));
-                    maquina01.buscarIdMaquina(usuarioDao.buscarIdUsuario(usuario));
-                    logado = true;
-
-                } else {
-                    System.out.println("Email ou senha incorretas. Tente novamente!");
+                        log("Login bem-sucedido para o usuário: " + usuario.getEmail());
+                    } else {
+                        System.out.println("Email ou senha incorretas. Tente novamente!");
+                    }
+                } catch (Exception e) {
+                    logError("Erro durante o processo de login", e);
                 }
             } while (!todasValidacoesLogin);
+
 
             System.out.println("Login realizado com sucesso! \uD83D\uDE04");
 
@@ -263,14 +321,19 @@ public class App {
 
                     // Imprima uma mensagem de sucesso no console
                     System.out.println("Dados de memória capturados e salvos com sucesso!");
+                    log("Dados de memória capturados e salvos com sucesso" + memoria);
                 } catch (Exception e) {
                     // Imprima quaisquer erros no console
                     e.printStackTrace();
                     System.err.println("Erro ao capturar e salvar dados de memória.");
+                    logError("Erro ao capturar e salvar dados de memória", e);
                 }
             }, 0, 10, TimeUnit.SECONDS);
         }
 
+        // Fechando os Scanners pra não derreter a memória (ou a RAM sei lá)
+        leitor.close();
+        leitorTexto.close();
     }
 }
 
