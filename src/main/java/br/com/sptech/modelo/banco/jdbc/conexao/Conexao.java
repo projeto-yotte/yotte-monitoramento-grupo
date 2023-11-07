d8fc4f6aeaa4efeceba68e0b381e51ebc7539d22
eusoubrian <brian.gomes@sptech.school>
2023-11-07 19:19:35 -0300

    Subindo o log na conexão no banco

--- src/main/java/br/com/sptech/modelo/banco/jdbc/conexao/Conexao.java
+++ src/main/java/br/com/sptech/modelo/banco/jdbc/conexao/Conexao.java
@@ -1,5 +1,6 @@
 package br.com.sptech.modelo.banco.jdbc.conexao;
 
+import br.com.sptech.modelo.banco.jdbc.App;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -8,18 +9,32 @@ public class Conexao {
     private JdbcTemplate conexaoDoBanco;
 
     public Conexao() {
-        BasicDataSource dataSource = new BasicDataSource();
+        try {
+            BasicDataSource dataSource = new BasicDataSource();
 
-        // Configurações para o MySQL
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/yotte");
-        dataSource.setUsername("yotte");
-        dataSource.setPassword("Yotte@42");
+            // Configurações para o MySQL
+            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            dataSource.setUrl("jdbc:mysql://localhost:3306/yotte");
+            dataSource.setUsername("yotte");
+            dataSource.setPassword("Yotte@42");
 
-        this.conexaoDoBanco = new JdbcTemplate(dataSource);
+            this.conexaoDoBanco = new JdbcTemplate(dataSource);
+
+            App.log("Conexão com o banco de dados estabelecida com sucesso.");
+        } catch (Exception e) {
+            App.logError("Erro ao estabelecer a conexão com o banco de dados.", e);
+        }
     }
 
     public JdbcTemplate getConexaoDoBanco() {
         return conexaoDoBanco;
+    }
+
+    public void log(String message) {
+        App.log(message);
+    }
+
+    public void logError(String errorMessage, Exception exception) {
+        App.logError(errorMessage, exception);
     }
 }
